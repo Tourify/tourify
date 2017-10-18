2d6b1c4263fa114831e3ee0ee496174efcc728d2
Katy Dillard <durhamdill@gmail.com>
2017-10-17 20:44:09 -0400

    add dynamic routes for home page

--- src/components/Baselayout.js
+++ src/components/Baselayout.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default class Baselayout extends React.Component {
+
   render(){
+
     return(
       <div>
         <nav><img src = "https://durhamdill.files.wordpress.com/2017/10/tourify-logo-200px.png" alt = "tourify logo"/></nav>
